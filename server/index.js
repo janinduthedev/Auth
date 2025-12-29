debe8707ea7585077bf516e46ff5a9d38f236a91
Janindu Tharinda <janindu.the.dev@gmail.com>
2025-12-29 21:46:04 +0530

    Configure basic middlewares and MongoDB connection

--- server/index.js
+++ server/index.js
@@ -4,5 +4,14 @@ import cors from 'cors';
 import mongoose from 'mongoose';
 
 const app = express();
-
 app.listen(5000, () => console.log("Server running on port 5000 🚀"));
+
+dotenv.config();
+
+app.use(express.json());
+
+app.use(cors());
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected... ✅"))
+  .catch(err => console.error("Connection Error: ❌", err));
